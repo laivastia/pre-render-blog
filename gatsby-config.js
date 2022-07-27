685d48f78d03e730939f7fe114576f9a5e372c9c
Vastia <zmfwl123@gmail.com>
2022-07-27 09:58:27 +0900

    gatsby setting
    setting for gatsby library
    create tsx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,50 +6,24 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "gatsby-plugin-typescript",
+      resolve: 'gatsby-plugin-typescript',
       options: {
         isTSX: true,
         allExtensions: true,
       },
     },
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `contents`,
-        path: `${__dirname}/contents`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `contents`,
+    //     path: `${__dirname}/contents`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
+    // To learn more, visit: <https://gatsby.dev/offline>
     // `gatsby-plugin-offline`,
   ],
-}
-
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: <https://www.gatsbyjs.com/docs/node-apis/>
- */
-
-// You can delete this file if you're not using it
-
-const path = require("path")
-
-// Setup Import Alias
-exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
-  const output = getConfig().output || {}
-
-  actions.setWebpackConfig({
-    output,
-    resolve: {
-      alias: {
-        components: path.resolve(__dirname, "src/components"),
-        utils: path.resolve(__dirname, "src/utils"),
-        hooks: path.resolve(__dirname, "src/hooks"),
-      },
-    },
-  })
 }
